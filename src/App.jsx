e7dbd39910f70c935829446ecb25df322e79b81c
Cboyce88 <95649151+Cboyce88@users.noreply.github.com>
2025-02-21 16:32:38 -0600

    socket

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@
 // import React from 'react'
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
-
+import { io } from "socket.io-client"; // Import Socket.IO
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
 
 import io from "socket.io-client";
 
-const socket = io.connect(`http://localhost:${4000}`);
+const socket = io("https://digitalmoleculemaker.onrender.com", {
+  transports: ["websocket"], // Ensures WebSockets are used
+});
 
 function App() {
   return (
